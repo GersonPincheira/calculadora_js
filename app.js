d4edbb2d8a70e5022befb42e703051ef349a7338
omuga <omuga@alumnos.inf.utfsm.cl>
2019-10-09 17:34:04 -0300

    Se completa las operaciones del archivo app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 var calculadora = {
     sumar: function sumarNumeros(n1, n2) {
-       //
+       number = n1 + n2;
+       return number;
     },
 
     restar: function restarNumeros(n1, n2) {
-       //
+       return n1 - n2;
     },
     
     multiplicar: function multiplicarNumeros(n1, n2) {
-        //
+        return n1 * n2;
     },
 
     dividir: function dividirNumeros(n1, n2) {
-        //
+        return (n1 / n2);
+        
     }
 
 
