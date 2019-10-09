83a98ade2555107e583aa229818af0c0781cb238
Gerson <gerson.pincheira@gmail.com>
2019-10-08 23:48:02 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var calculadora = {
+    sumar: function sumarNumeros(n1, n2) {
+       //
+    },
+
+    restar: function restarNumeros(n1, n2) {
+       //
+    },
+    
+    multiplicar: function multiplicarNumeros(n1, n2) {
+        //
+    },
+
+    dividir: function dividirNumeros(n1, n2) {
+        //
+    }
+
+
+};
